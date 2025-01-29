20698c46ccfaa4b6cf685d3a85ec99300b317b0f
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2025-01-29 15:16:29 -0300

    chore: upgrade global-wallet-client to v4.4.2

--- src/lib/registerSolanaStandard.ts
+++ src/lib/registerSolanaStandard.ts
@@ -1,6 +1,8 @@
-import { registerWallet } from "@wallet-standard/wallet";
 import Wallet from "./wallet";
-import { createSolanaWallet } from "@dynamic-labs/global-wallet-client/solana";
+import {
+  createSolanaWallet,
+  registerWallet,
+} from "@dynamic-labs/global-wallet-client/solana";
 import { config } from "./config";
 
 export const registerSolanaStandard = () => {
